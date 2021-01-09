24dc56cdd88870eb658c6e600356a8244bbf4d10
thevyanshu <divyanshujain20010808@gmail.com>
2021-01-09 22:57:03 +0530

    database updated

--- domo/domo/settings.py
+++ domo/domo/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'domo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'domo_app',
+        'USER': 'root',
+        'PASSWORD': 'divyanshu08',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
